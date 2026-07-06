6b5c77bd4d11f82ada36f220919e796ea6bf42ec
heshankav <heshankavinda2023@gmail.com>
2026-07-06 19:31:20 +0530

    include the funtion

---
+++ server/services/patientService.js
@@ -0,0 +1,41 @@
+const PatientProfile = require('../models/PatientProfile');
+
+const getPatientDashboardData = async (userId, userEmail) => {
+   
+    const profile = await PatientProfile.findOne({ user: userId });
+
+    if (!profile) {
+        throw new Error('Patient profile not found'); 
+    }
+
+    const latestPrescriptionData = profile.prescriptions.length > 0 
+        ? profile.prescriptions[profile.prescriptions.length - 1].medications 
+        : [];
+
+    return {
+        healthCard: {
+            fullName: profile.fullName,
+            patientId: profile.patientId,
+            bloodGroup: profile.bloodGroup,
+            dob: profile.dob,
+            gender: profile.gender,
+            phone: profile.phone,
+            qrCodeData: profile.qrCodeData
+        },
+        personalInformation: {
+            patientId: profile.patientId,
+            phone: profile.phone,
+            email: userEmail, 
+            address: profile.address,
+            gender: profile.gender,
+            dob: profile.dob
+        },
+        medicalHistory: profile.medicalHistory,
+        latestPrescriptions: latestPrescriptionData,
+        emergencyContact: profile.emergencyContact
+    };
+};
+
+module.exports = {
+    getPatientDashboardData
+};
