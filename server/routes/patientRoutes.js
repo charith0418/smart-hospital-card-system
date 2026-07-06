995b055e28ae79ec55dc3728a90482c55e0afaa3
heshankav <heshankavinda2023@gmail.com>
2026-07-06 20:11:44 +0530

    update code

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getDashboardData } = require('../controllers/patientController');
+const { getDashboard } = require('../controllers/patientController');
 const { protect, patientOnly } = require('../middleware/authMiddleware');
 
-router.get('/dashboard', protect, patientOnly, getDashboardData);
+router.get('/dashboard', protect, patientOnly, getDashboard);
 
 module.exports = router;
