6c79399ba977cf0845238ef544e33e3996f02719
heshankav <heshankavinda2023@gmail.com>
2026-07-12 12:35:16 +0530

    update patients routes file

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const { getDashboard ,createTestProfile ,downloadQRCode ,getMedicalHistory} = require('../controllers/patientController');
 const { protect, patientOnly } = require('../middleware/authMiddleware');
 
 router.post('/test-profile', protect, patientOnly, createTestProfile);
 router.get('/dashboard', protect, patientOnly, getDashboard);
-router.get('/qr-code', protect, patientOnly, downloadQRCode);
+router.get('/download-qr', protect, patientOnly, downloadQRCode);
 router.get('/medical-history', protect, patientOnly, getMedicalHistory);
 
 module.exports = router;
