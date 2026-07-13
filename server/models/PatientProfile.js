86ded0d18551cdc6bb53ab76a9f2b227f8134cac
heshankav <heshankavinda2023@gmail.com>
2026-07-13 23:47:14 +0530

    update model files

--- server/models/PatientProfile.js
+++ server/models/PatientProfile.js
@@ -31,10 +31,17 @@ const patientProfileSchema = new mongoose.Schema({
         }]
     },
     prescriptions: [{
+        prescriptionId: { type: String }, 
+        doctorName: { type: String },     
+        hospital: { type: String },       
+        diagnosis: { type: String },      
+        instructions: { type: String },  
+        dateIssued: { type: Date, default: Date.now },
         medications: [{
-            name: { type: String },               
-            dosageInstructions: { type: String }, 
-            duration: { type: String }            
+            name: { type: String },       
+            dosage: { type: String },    
+            frequency: { type: String },  
+            duration: { type: String }    
         }],
         dateIssued: { type: Date, default: Date.now }
     }],
